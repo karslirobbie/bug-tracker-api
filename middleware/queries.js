039930e3e9ca5036deb1623eac5fe412db31ed65
Robbie Karsli <clementerobbie@gmail.com>
2021-05-11 10:29:47 +0800

    Ticket Query Params accept Object Id

--- middleware/queries.js
+++ middleware/queries.js
@@ -2,9 +2,13 @@
 
 const queryString = (req, res, next) => {
   const query = {}
+  const objectIds = ["assignedTo", "assignee", "createdBy"];
 
   for (const [prop, val] of Object.entries(req.query)) {
-    query[prop] = new RegExp(".*" + val + ".*", "i")
+
+    if (objectIds.includes(prop)) query[prop] = val
+    else query[prop] = new RegExp(".*" + val + ".*", "i")
+
   }
 
   res.locals.query = query
